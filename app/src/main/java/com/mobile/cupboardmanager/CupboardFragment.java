49c0cb65dd66eaad8acb1ae2d09ba9f0e85ddf95
matt-auld <matthew.william.auld@gmail.com>
2014-12-03 15:45:44 +0000

    Implement add new item behaviour
    This will start the ItemActivity passing the relevant ITEM_TYPE information for
    each of the Shopping and Cupboard Fragments

--- app/src/main/java/com/mobile/cupboardmanager/CupboardFragment.java
+++ app/src/main/java/com/mobile/cupboardmanager/CupboardFragment.java
@@ -1,25 +1,30 @@
 package com.mobile.cupboardmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 /**
- * Created by Lawrence on 30/11/2014.
+ * Display interactive list of CupboardItems, with creation button
  */
 public class CupboardFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_cupboard, container, false);
+        rootView.findViewById(R.id.add_cupboard_item).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ItemActivity.class);
+                intent.putExtra(ItemActivity.ITEM_TYPE_INTENT_EXTRA,
+                        ItemActivity.ITEM_CUPBOARD_TYPE);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
