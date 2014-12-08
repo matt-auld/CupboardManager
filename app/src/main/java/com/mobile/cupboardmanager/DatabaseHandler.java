5749ef4fe4ec37ebe3d9230cdde2bf8eae92c678
matt-auld <matthew.william.auld@gmail.com>
2014-12-08 23:23:15 +0000

    Remove *Item classes

--- app/src/main/java/com/mobile/cupboardmanager/DatabaseHandler.java
+++ app/src/main/java/com/mobile/cupboardmanager/DatabaseHandler.java
@@ -1,16 +1,8 @@
 package com.mobile.cupboardmanager;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteQueryBuilder;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 /**
  *  Database helper-class implementing CRUD(Create, Read, Update, Delete)
@@ -83,113 +75,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         mContext.deleteDatabase(DATABASE_NAME);
-    }
-
-    //insert item into database
-    public long insertItem(Item item) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(ITEM_NAME, item.getName());
-
-        long item_id = db.insert(ITEM_TABLE, null, values);
-
-        return item_id;
-    }
-
-    //insert shopping item into database
-    public long insertItem(ShoppingItem shoppingItem) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        // We need to find the ID for the item in the items table
-
-        // Create a builder to query database
-        SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
-        qBuilder.setTables(ITEM_TABLE);
-        qBuilder.appendWhere(ITEM_NAME + "=\"" + shoppingItem.getName() + "\"");
-        String[] projection = {ITEM_ID};
-
-        Cursor cursor = qBuilder.query(db, projection, null, null, null, null, null);
-
-
-        if (cursor.getCount() < 1) {
-            throw new NoSuchElementException("No item found in the Items table with the name of: " +
-                    shoppingItem.getName() + "\" when trying to insert a shopping item");
-        } else if (cursor.getCount() > 1) {
-            Log.w(TAG, "Multiple items found for name: " + shoppingItem.getName() +
-                    "\" when trying to insert a shopping item");
-        }
-
-        cursor.moveToNext();
-        int idIndex = cursor.getColumnIndex(ITEM_ID);
-
-        int itemID = cursor.getInt(idIndex);
-
-        ContentValues values = new ContentValues();
-        values.put(SHOPPING_QUANTITY, shoppingItem.getQuantity());
-        values.put(SHOPPING_ITEM, itemID);
-
-        long shopping_item_id = db.insert(SHOPPING_TABLE, null, values);
-
-        return shopping_item_id;
-    }
-
-    //fetch all shopping items, as a list
-    public List<ShoppingItem> fetchAllShoppingItems() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        List<ShoppingItem> shoppingItems = new ArrayList<ShoppingItem>();
-
-        String selectQuery = "SELECT  * FROM " + SHOPPING_TABLE;
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        if (c.moveToFirst()) {
-            do {
-                ShoppingItem item = new ShoppingItem();
-                item.setQuantity(c.getInt((c.getColumnIndex(SHOPPING_QUANTITY))));
-                item.setName((c.getString(c.getColumnIndex(SHOPPING_ITEM))));
-                shoppingItems.add(item);
-            } while (c.moveToNext());
-        }
-
-        return shoppingItems;
-    }
-
-    //insert shopping item into database
-    public long insertItem(CupboardItem cupboardItem) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        // We need to find the ID for the item in the items table
-
-        // Create a builder to query database
-        SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
-        qBuilder.setTables(ITEM_TABLE);
-        qBuilder.appendWhere(ITEM_NAME + "=\"" + cupboardItem.getName() + "\"");
-        String[] projection = {ITEM_ID};
-
-        Cursor cursor = qBuilder.query(db, projection, null, null, null, null, null);
-
-        if (cursor.getCount() < 1) {
-            throw new NoSuchElementException("No item found in the Items table with the name of: \"" +
-                    cupboardItem.getName() + "\" when trying to insert a cupboard item");
-        } else if (cursor.getCount() > 1) {
-            Log.w(TAG, "Multiple items found for name: " + cupboardItem.getName() +
-                    "\" when trying to insert a cupboard item");
-        }
-
-        cursor.moveToNext();
-        int idIndex = cursor.getColumnIndex(ITEM_ID);
-
-        int itemID = cursor.getInt(idIndex);
-
-        ContentValues values = new ContentValues();
-        values.put(CUPBOARD_ITEM, itemID);
-        values.put(CUPBOARD_QUANTITY, cupboardItem.getQuantity());
-        values.put(CUPBOARD_EXPIRY_TIME, cupboardItem.getExpiry_time_ms());
-        values.put(CUPBOARD_NOTIFICATION_ID, cupboardItem.getNotification_id());
-
-        long cupboard_item_id = db.insert(CUPBOARD_TABLE, null, values);
-
-        return cupboard_item_id;
     }
 }
 
